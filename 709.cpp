9961ed587be51012a8970580cb5e9cca076f4af1
deukyoon <77385179+deukyoon@users.noreply.github.com>
2022-04-07 19:26:36 +0900

    To Lower Case

---
+++ 709.cpp
@@ -0,0 +1,15 @@
+class Solution 
+{
+
+    public:
+    string toLowerCase(string s) 
+    {
+        string ans = "";
+        
+        for (auto& c : s)
+            
+            ans += c > 64 && c < 91 ? c + 32 : c;
+        
+        return ans;
+    }
+};
